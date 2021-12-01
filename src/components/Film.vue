01d3fa2b1210d6aad60a7d63a0dd948625a3de48
GianniPio <giannipiopellegrino00@gmail.com>
2021-12-01 14:48:10 +0100

    created props and method for print films

--- src/components/Film.vue
+++ src/components/Film.vue
@@ -1,16 +1,31 @@
 <template>
-  <div>
-    
+  <div class="elefilm">
+    <span>{{details.title}}</span>
+    <span>{{details.original_title}}</span>
+    <span>{{details.original_language}}</span>
+    <span>{{details.vote_average}}</span>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Film',
+  props: {
+      details: Object,
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
+    .elefilm {
+
+        width: calc(100% / 3);
+
+        img {
+            border: 1px solid white;
+        }
+    }
 
 </style>
--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,18 +1,33 @@
 <template>
   <main>
-    <Film/>
+    <Film v-for="film, i in listfilms" :key="i" :details="film"/>
   </main>
 </template>
 
 <script>
 
 import Film from '@/components/Film.vue';
+import axios from "axios";
 
 
 export default {
   name: 'MyMain',
   components: {
     Film,
+  },
+  data() {
+    return {
+      listfilms: [],
+      APIfilm: "https://api.themoviedb.org/3/search/movie?api_key=dd75a648bab4fb373b26c8914bfc572a&query=thor&language=it-IT"
+    }
+  },
+  created() {
+    this.getFilms();
+  },
+  methods: {
+    getFilms() {
+      axios.get(this.APIfilm).then((result) => this.listfilms = result.data.results)
+    }
   }
 }
 </script>
@@ -21,7 +36,7 @@ export default {
 <style scoped lang="scss">
 
 main {
-  height: 100vh;
+  padding: 15px;
   background-color: grey;
 }
 </style>
